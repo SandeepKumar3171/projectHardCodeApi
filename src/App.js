bd62d7c0118c265334d22ad4e18f9807a636e7e9
SandeepKumar3171 <sandeepidkk@gmail.com>
2023-10-28 17:29:07 +0530

    add all file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import Product from "./components/Product";
 
 function App() {
+
+  let mydata = {
+    "data": [
+      {
+        "name": "Cosmetics",
+        "productList": [
+          {
+            "name": "Hair Oil",
+            "price": 122
+          },
+          {
+            "name": "Face wash",
+            "price": 123
+          }
+        ]
+      },
+      {
+        "name": "Household",
+        "productList": [
+          {
+            "name": "Hair Oil",
+            "price": 122
+          },
+          {
+            "name": "Face wash",
+            "price": 123
+          }
+        ]
+      }
+    ]
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="md:m-5 flex flex-col justify-between p-5">
+        {
+          mydata.data && mydata.data.map((item, index) => (
+            <div key={index}>
+              <p className="border-b-4 border-gray-700 m-2 text-xl font-semibold">{item.name}</p>
+              <div className="flex md:flex-row flex-col w-full gap-5 md:m-5">
+
+                {item.productList && item.productList.map((e,ind) => (
+
+                  <div key={ind} className="md:w-1/2 md:ml-10 md:mr-10 w-full">
+                      <Product
+                      name={e.name}
+                      price={e.price}
+                    />
+                    </div>
+                ))}
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Product.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Product = ({ name, price }) => {
+
+    const addHandler=()=>{
+        alert("Add data in cart")
+        console.log("name:",name,"\n","price:",price)
+
+    }
+    const removeHandler=()=>{
+        alert("Remove data from cart")
+        console.log("Remove data from cart","\n","name:",name,"\n","price:",price)
+
+    }
+    return (
+
+        <div className='flex flex-col bg-slate-500 md:justify-center p-5 justify-evenly md:ml-5 md:mr-5 text-white'>
+            <p className='font-semibold'>Name: {name}</p>
+            <p className='font-semibold '>Price:{price}</p>
+            <div className='flex flex-col gap-2 items-center w-full'>
+                <button onClick={addHandler} className='border bg-blue-700 p-1 rounded-lg w-full'>Add to the cart</button>
+                <button onClick={removeHandler} className='border bg-red-700 p-1 rounded-lg w-full'>Remove from cart</button>
+            </div>
+        </div>
+    )
+}
+
+export default Product
